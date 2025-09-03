37542d8ea217bf8368b6cac297e23b334ef8fce1
codewithahasanul <ahasanulhaque204@gmailcom>
2025-09-03 19:08:57 +0600

    task 2 completed

--- js-task.js
+++ js-task.js
@@ -7,4 +7,13 @@ let applePrice = 350; // Price/kg
 
 let totalCost = orangePrice + applePrice;
 let changeMoney = totalTaka - totalCost;
-console.log("Shopkeeper will return:", changeMoney);
+console.log("Shopkeeper will return:", changeMoney); // Output is here
+
+
+//Task-2
+//Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+//Input:
+//The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+//Output:
+//Print the result in 2 decimal places.
