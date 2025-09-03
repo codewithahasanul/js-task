241a73bfecfd9c8114460512340791459cff27e2
codewithahasanul <ahasanulhaque204@gmailcom>
2025-09-03 17:36:14 +0600

    changed something

---
+++ js-task.js
@@ -0,0 +1,10 @@
+// Task-1
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+let totalTaka = 1000;
+let orangePrice = 350; // Price/kg
+let applePrice = 350; // Price/kg
+
+let totalCost = orangePrice + applePrice;
+let changeMoney = totalTaka - totalCost;
+console.log("Shopkeeper will return:", changeMoney);
